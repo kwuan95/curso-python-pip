83a5505e4740af5413b96ffed14fdf07dbb0f962
kwuan95 <juserora95@gmail.com>
2023-01-30 00:52:01 -0500

    projecto werserver

---
+++ web-server/store.py
@@ -0,0 +1,9 @@
+import requests
+def get_categories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categorias = r.json()
+    for category in categorias:
+        print(category['name'])
