981081726a1a7dbcdcf1bfb6b3859aa55e528840
kwuan95 <juserora95@gmail.com>
2023-01-30 01:43:00 -0500

    cosas en app y inciando nuevo en web

--- app/charts.py
+++ app/charts.py
@@ -8,7 +8,7 @@ def generate_bar_chart(name, labels, values):
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
-  ax.pie(values, labels=labels)
+  plt.pie(values, labels=labels)
   ax.axis('equal')
   plt.savefig('pie.png')
   plt.close()
