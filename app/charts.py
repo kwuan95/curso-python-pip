c3b58e279785f2e9f685aa6eeae062bba58ca23b
kwuan95 <juserora95@gmail.com>
2023-02-06 22:26:56 -0500

    agregar dodker y varios archivos

--- app/charts.py
+++ app/charts.py
@@ -10,7 +10,7 @@ def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   plt.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig('pie.png')
+  plt.savefig('chart_pie_paraverendocker.png')
   plt.close()
 
 if __name__ == '__main__':
